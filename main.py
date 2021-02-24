6987979ea6646242db976e2c693d51829c598ba9
blackpc <xou.xavl@gmail.com>
2021-02-24 21:30:46 +0100

    Personnages, capacités

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("!! Gauntlet !!")
+import Character as m_character
+import Capacity as m_capacity
+
+if __name__ == "__main__":    
+    print("!! Gauntlet !!")
+
+    # Test
+    ## Character
+    c = m_character.Character("character")
+    z = m_character.Wizard("wizard")
+    w = m_character.Warrior("warrior")
+    e = m_character.Elf("elf")
+
+    l = [c, z, w, e]
+
+    for character in l:
+        print(character)
+
+    ## Capacity
+    c.addCapacity(m_capacity.Capacity("c_jump"))
+    c.addCapacity(m_capacity.Capacity("c_run"))
+    z.addCapacity(m_capacity.Capacity("z_speak"))
+    w.addCapacity(m_capacity.Capacity("w_fight"))
+    e.addCapacity(m_capacity.Capacity("e_fast"))
+
+    for character in l:
+        print(f"{character}")
+        character.listCapacity()
